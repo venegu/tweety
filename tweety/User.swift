dd12f84afbba280c969b62d3594c8d5759bc9283
venegu <venegu@gmail.com>
2016-03-03 14:26:02 -0500

    Updating user model

--- tweety/User.swift
+++ tweety/User.swift
@@ -12,9 +12,17 @@ class User: NSObject {
     
     var name: NSString?
     var screenname: NSString?
-    var profileUrl: NSURL?
+    var profileUrlLow: NSURL?
+    var profileUrlHigh: NSURL?
+    var profileBannerImage: NSURL?
+    var profileBackgroundImage: NSURL?
     var tagline: NSString?
     var dictionary: NSDictionary?
+    var id: Int?
+    var tweetsCount: Int?
+    var likesCount: Int?
+    var followingCount: Int?
+    var followersCount: Int?
     
     // Constructor to deserialize properties retrieved from the API
     init(dictionary: NSDictionary) {
@@ -24,10 +32,18 @@ class User: NSObject {
         let profileUrlString = dictionary["profile_image_url_https"] as? String
         
         if let profileUrlString = profileUrlString {
-            profileUrl = NSURL(string: profileUrlString)
+            profileUrlLow = NSURL(string: profileUrlString)
+            profileUrlHigh = NSURL(string: profileUrlString.stringByReplacingOccurrencesOfString("_normal", withString: ""))
         }
         
         tagline = dictionary["description"] as? String
+        id = dictionary["id"] as? Int
+        
+        tweetsCount = dictionary["statuses_count"] as? Int
+        likesCount = dictionary["favourites_count"] as? Int
+        followingCount = dictionary["friends_count"] as? Int
+        followersCount = dictionary["followers_count"] as? Int
+        
     }
     
     static var _currentUser: User?
